9f006cce0f906b24daaf03941a92c85ca5294bd9
zachblume <zachblume@gmail.com>
2023-02-10 17:11:53 -0500

    no loading flash

--- app/pages/index.js
+++ app/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   useEffect(() => {
     if (query !== "") {
-      setLoading(true);
+      // setLoading(true);
       let url = "/api/forwardme?query=" + query;
       let response = fetch(url);
       response.then(async (res) => {
