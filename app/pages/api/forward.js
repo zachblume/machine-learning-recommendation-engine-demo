78a81471026dd6000c1a94fedf00c2f360a5f0b2
zachblume <zachblume@gmail.com>
2023-02-10 10:30:29 -0500

    no api

--- app/pages/api/forward.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
-  let results = await fetch("http://34.86.228.54/?query=" + req.query.query);
-  res.status(200).send(await results.text());
-}
